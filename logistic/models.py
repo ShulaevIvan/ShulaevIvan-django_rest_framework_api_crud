2fc46aa112e945a457ee745cc0e0601ad3c7d4bb
Shulaev <mail@ntdservice.ru>
2022-05-04 12:37:13 +0300

    clear

--- logistic/models.py
+++ logistic/models.py
@@ -3,16 +3,19 @@ from django.db import models
 
 
 class Product(models.Model):
+
     title = models.CharField(max_length=60, unique=True)
     description = models.TextField(null=True, blank=True)
 
 
 class Stock(models.Model):
+
     address = models.CharField(max_length=200, unique=True)
     products = models.ManyToManyField(Product, through='StockProduct', related_name='stocks', )
 
 
 class StockProduct(models.Model):
+    
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name='positions',)
     product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='positions',)
     quantity = models.PositiveIntegerField(default=1)
